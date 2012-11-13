9aaee939e4b7e22217ee78f20ab8be4900c44346
Jingzhe <changsha.zjz@gmail.com>
2012-11-13 13:28:49 -0500

    muti-contract

--- Contract/Contract.cpp
+++ Contract/Contract.cpp
@@ -1,110 +1,199 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
+#include <iomanip>
 
 using namespace std;
-
+#define MAX_ITEMS 100
 class Contacts
 {
-	string firstName;
-	string lastName;
-	string emailID;
-	string phoneNumber;
+	string firstName[MAX_ITEMS];
+	string lastName[MAX_ITEMS];
+	string emailID[MAX_ITEMS];
+	string phoneNumber[MAX_ITEMS];
 	ofstream myfilewriter;
 	ifstream myfilereader;
+
+	int index;
 public:
 	Contacts() 
 	{ //constructor
-		firstName = "";
-		lastName = "";
-		emailID = "";
-		phoneNumber = "";
+		//firstName = "";
+		//lastName = "";
+		//emailID = "";
+		//phoneNumber = "";
+		index = 0;
 	}
-	void createContact() 
+
+	void createContact(int _index) 
 	{
 		cout <<"Enter first name:" << endl;
-		cin >> firstName;
+		cin >> firstName[_index];
 
 		cout <<"Enter last name:" << endl;
-		cin >> lastName;
+		cin >> lastName[_index];
 
 		cout << "Enter email address" << endl;
-		cin >> emailID;
+		cin >> emailID[_index];
 
-		cout << "Enter phone number" << endl;
-		cin >> phoneNumber;
+		cout << "Enter phone number" <<endl;
+		cin >> phoneNumber[_index];
+		index++;
 	}
+
+	void deleteContact()
+	{
+		displayContact();
+		int delIndex=0;
+		char conform;
+		do{
+			cout << "which one? (1-" << index << ")  "  ;
+			cin.clear();
+			cin.sync();  
+			cin >> delIndex;
+		}while(cin.fail());
+
+		do{
+			cout << "Are you sure? (Y/N) "  ;
+			cin.clear();
+			cin.sync();  
+			cin >> conform;
+		}while(cin.fail());
+
+		if(conform=='N'||conform=='n') return ;
+		if(conform=='Y'||conform=='y')
+		{
+			delIndex--;
+			for(int i=delIndex; i<index-1; i++)
+			{
+				firstName[i] = firstName[i+1];
+				lastName[i] = lastName[i];
+				emailID[i] = emailID[i+1];
+				phoneNumber[i] = phoneNumber[i+1];
+			}
+			index--;
+		}
+	}
+
+	int getIndex()
+	{
+		return index;
+	}
+
 	void displayContact() 
 	{
-		cout <<"---------------Display contact information---------------" << endl;
-		cout << "1. First name: " << firstName << endl;
-		cout << "2. Last name:" << lastName << endl;
-		cout << "3. Email ID: " << emailID << endl;
-		cout << "4. Phone Number: " << phoneNumber << endl
-			<<"----------------------------------------------------------" << endl <<endl;
+			cout << setw (12)  << " First Name" << " | " 
+				 << setw (12)  << " Last Name " << " | " 
+				 << setw (20)  << " Email ID" << " | " 
+				 << setw (14)  << " Phone Number" << endl;
+			cout << "------------------------------------------------------------------------"<<endl;
+		for(int i=0; i<index; i++)
+		{
+			//cout <<"---------------Display contanct information---------------" << endl;
+			//cout << "1. First name: " << firstName[i] << endl;
+			//cout << "2. Last name:" << lastName[i] << endl;
+			//cout << "3. Email ID: " << emailID[i] << endl;
+			//cout << "4. Phone Number: " << phoneNumber[i] << endl
+			//	 <<"----------------------------------------------------------" << endl <<endl;
+
+
+
+			cout << setw (12)  << firstName[i] << " | " 
+				 << setw (12)  << lastName[i]  << " | " 
+				 << setw (20)  << emailID[i]   << " | " 
+				 << setw (14)  << phoneNumber[i] << endl;
+		}
 	}
 
 	void editContact() 
 	{
-		displayContact();
-		cout << "Choose one that you want to edit (1-4): ";
 		int editItem =0;
-		cin >> editItem;
+		displayContact();
+		int editIndex=0;
+		do{
+			cout << "which one? (1-" << index << ")  " ;
+			cin.clear();
+			cin.sync();  
+			cin >> editIndex;
+		}while(cin.fail());
+
+		editIndex--;
+		do{
+			cout << endl<< "Chooce one that you want to edit (1,First Name; 2,Last Name; 3,email; 4, Phone#): ";
+			cin.clear();
+			cin.sync();  
+			cin >> editItem;
+		}while(cin.fail());
+
 		switch(editItem)
 		{
 		case 1:
 			cout << "enter new first name:" << endl;
-			cin >> firstName;
+			cin >> firstName[editIndex];
 			break;
 		case 2:
 			cout << "enter new last name:" << endl;
-			cin >> lastName;
+			cin >> lastName[editIndex];
 			break;
 		case 3:
 			cout << "enter new email address:" << endl;
-			cin >> emailID;
+			cin >> emailID[editIndex];
 			break;
 		case 4:
 			cout << "enter new phone number:" << endl;
-			cin >> phoneNumber;
+			cin >> phoneNumber[editIndex];
 			break;
 		default:
 			cout << "Wrong choice, please enter again..." << endl;
-			editContact();
 		}
 	}
 
 	void save()
 	{
-		myfilewriter.open("contract.txt");
-		myfilewriter 	/*<< "First name: "*/ << firstName << endl
-			/*<< "Last name:"*/ << lastName << endl
-			/*<< "Email ID: "*/ << emailID << endl
-			/*<< "Phone Number: "*/ << phoneNumber << endl;
-		myfilewriter.close();
+		char conform;
+		do{
+			cout << "save? (Y/N) "  ;
+			cin.clear();
+			cin.sync();  
+			cin >> conform;
+		}while(cin.fail());
+		if(conform=='Y'||conform=='y')
+		{
+			myfilewriter.open("contract.txt");
+			for(int i=0; i<index; i++)
+			{
+				myfilewriter 	/*<< "First name: "*/ << firstName[i] << endl
+				/*<< "Last name:"*/ << lastName[i] << endl
+				/*<< "Email ID: "*/ << emailID[i] << endl
+				/*<< "Phone Number: "*/ << phoneNumber[i] << endl;
+			}
+			myfilewriter.close();
+		}
 	}
 
 	bool load()
 	{
 		cout << "Load contract..."<<endl;
 		myfilereader.open("contract.txt");
+		string d="";
 		if(myfilereader.is_open())
 		{
-			if(!myfilereader.eof())
+			while(!myfilereader.eof())
 			{
-				getline(myfilereader, firstName);
-				getline(myfilereader, lastName);
-				getline(myfilereader, emailID);
-				getline(myfilereader, phoneNumber);
-				cout << "Contract has been loaded" <<endl;
-				myfilereader.close();
-				return true;
+				getline(myfilereader, firstName[index]);
+				getline(myfilereader, lastName[index]);
+				getline(myfilereader, emailID[index]);
+				getline(myfilereader, phoneNumber[index]);
+				index++;
 			}
+			index--;
+			cout << index <<" contract has been loaded" <<endl;
+			myfilereader.close();
+			return true;
 		}
 		else 
 		{
-			cout << "no contract exists, create a new one..." <<endl;
+			cout << "No contract exists, create a new one..." <<endl;
 			return false;
 		}
 	}
@@ -114,29 +203,38 @@ int main ()
 {
 
 	Contacts ourAddressBook;
-
-	/*cout <<"Creating a new contact" << endl;*/
+	int choice;
 
 	if (!ourAddressBook.load())
-		ourAddressBook.createContact();
-
-
+		ourAddressBook.createContact(0);
 
 	while(1) 
 	{
 		cout <<endl;
-		cout << "1. Display contact" << endl;
-		cout << "2. Edit contact" << endl;
-		cout << "3. Save and Exit" << endl;
-		int choice;
-		cin >> choice;
+		do{
+			cout << "1. Display contact" << endl;
+			cout << "2. Edit contact" << endl;
+			cout << "3. New contact" <<endl;
+			cout << "4. Delete contact" <<endl;
+			cout << "5. Save and Exit" << endl;
+		
+			cin.clear();
+			cin.sync();  
+			cin >> choice;
+		}while(cin.fail());
 
 		switch(choice) {
 		case 1: ourAddressBook.displayContact();
 			break;
 		case 2: ourAddressBook.editContact();
 			break;
-		case 3:
+		case 3: 
+			ourAddressBook.createContact(ourAddressBook.getIndex());
+			break;
+		case 4: 
+			ourAddressBook.deleteContact();
+			break;
+		case 5:
 			ourAddressBook.save();
 			exit(1);		
 		}
